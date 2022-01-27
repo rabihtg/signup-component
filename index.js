04d375f5185749cd6dce7f60bacb011097546d78
rabih harb <rabiharb.ss@outlook.com>
2022-01-27 19:30:55 +0200

    fix email validation, use reg. exp.

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// Done - Set max char for each email part
+// Done - Check the problem with regex taking so long to compute on long domain names
+
 const userForm = document.querySelector("form");
 const userInputs = document.querySelectorAll("input");
 
-// const userFirstNameInput = document.querySelector("#firstName");
-// const userLastNameInput = document.querySelector("#lastName");
 const userEmailInput = document.querySelector("#email");
-// const userPasswordInput = document.querySelector("#password");
 
 userForm.addEventListener("submit", function (e) {
   e.preventDefault();
   userInputs.forEach((input) => {
-    if (input.value.trim() === "") {
+    let realValue = input.value;
+    if (input.getAttribute("data-details") !== "Password") {
+      realValue = input.value.trim();
+    }
+    if (realValue === "") {
       input.setAttribute("aria-invalid", true);
       input.setAttribute("placeholder", "");
       input.nextElementSibling.nextElementSibling.innerText =
@@ -19,15 +23,17 @@ userForm.addEventListener("submit", function (e) {
       input.setAttribute("placeholder", input.getAttribute("data-details"));
       input.nextElementSibling.nextElementSibling.innerText = "";
     }
+
+    console.log(input.value);
   });
   if (userEmailInput.getAttribute("aria-invalid") !== "true") {
     const emailValue = userEmailInput.value;
+    const validEmailRegx =
+      /(^[a-zA-Z0-9]{1,}([!#$%&'*+-/=_`?^{|.][a-zA-Z0-9]{1,})*?)+@([a-zA-Z0-9]([-.][a-zA-Z0-9])*){1,}\.[a-zA-Z]{2,3}$/g;
     if (
-      (emailValue.match(/@/g) || []).length !== 1 ||
-      emailValue.split(".").length !== 2 ||
-      emailValue.split("@")[0].trim() === "" ||
-      emailValue.split("@")[1].split(".")[0].trim() === "" ||
-      emailValue.split(".")[1] !== "com"
+      !emailValue.match(validEmailRegx) ||
+      emailValue.split("@")[0].length > 64 ||
+      emailValue.split("@")[1].split(".")[0].length > 253
     ) {
       userEmailInput.setAttribute("aria-invalid", true);
       userEmailInput.nextElementSibling.nextElementSibling.innerText =
